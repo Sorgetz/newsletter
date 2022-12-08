a64548ebb343191b3192ba2b3cd8b259356b9405
Sorgetz <eduardasorgetzalves@gmail.com>
2022-12-07 21:00:53 -0300

    Change email html to better show the news

--- src/main/java/com/sorgetz/newslettertask/service/SchedulerService.java
+++ src/main/java/com/sorgetz/newslettertask/service/SchedulerService.java
@@ -1,12 +1,15 @@
 package com.sorgetz.newslettertask.service;
 
 
+import com.sorgetz.newslettertask.model.News;
 import com.sorgetz.newslettertask.model.Users;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -25,41 +28,44 @@ public class SchedulerService {
     private static final String TIME_ZONE = "America/Sao_Paulo";
 
 //    @Scheduled(cron = "0 0 8 * * *", zone = TIME_ZONE)
+    @Scheduled(cron = "0 5 0 * * *", zone = TIME_ZONE)
     public void sendNewsEmail(){
 
-        log.info("Starting to send emails to users at " + LocalDate.now());
+        log.info("Starting to send emails to users at " + LocalDateTime.now());
 
         StringBuilder newsText = new StringBuilder();
         newsText.append("<p> Segue as notícias de hoje. </p>");
 
-        this.newsService.getAllNotSentNews().forEach(news -> {
+        List<News> newsList = this.newsService.getAllNotSentNews();
 
-            String title = news.getLink() != null
-                    ? String.format("<a href=\"%s\"> %s </a>", news.getLink(), news.getTitle())
-                    : news.getTitle();
+        if(!newsList.isEmpty()) {
+            newsList.forEach(news -> {
 
-            newsText.append(String.format(
-                            "<p> <b> %s </b> </p>" +
-                            "%s" , title, news.getDescription()));
+                String title = news.getLink() != null
+                        ? String.format("<a href=\"%s\"> %s </a>", news.getLink(), news.getTitle())
+                        : news.getTitle();
 
-            this.newsService.updateNewsSent(news, Boolean.TRUE);
+                newsText.append(String.format(
+                        "<p> <b> %s </b> <br /> %s </p>", title, news.getDescription()));
 
-        });
+                this.newsService.updateNewsSent(news, Boolean.TRUE);
+            });
 
-        List<Users> users = this.userService.getAllUsers();
+            this.userService.getAllUsers().forEach(user -> {
 
-        users.forEach(user -> {
+                String subject = "Notícias do dia!";
+                String birthday = this.userService.isUserBirthday(user) ? "Feliz aniversário!!!" : "";
+                String body = String.format(
+                                "<p>Bom dia %s! %s</p> %s" +
+                                "<p> Até a próxima.</p>", user.getName(), birthday, newsText);
 
-            String subject = "Notícias do dia!";
-            String birthday = this.userService.isUserBirthday(user) ? "Feliz aniversário" : "";
-            String body = String.format(
-                    "<p>Bom dia %s! %s</p> %s", user.getName(), birthday, newsText);
+                this.mailService.sendMail(user.getEmail(), subject, body);
 
-            this.mailService.sendMail(user.getEmail(), subject, body);
+                log.info("Sending email to " + user.getEmail());
 
-            log.info("Sending email to " + user.getEmail());
+            });
 
-        });
+        }else log.info("No news today");
 
     }
 
